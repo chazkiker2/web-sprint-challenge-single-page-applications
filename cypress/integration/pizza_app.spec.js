f77652d5d715ae188e79a3c130d4f4d8669a529b
chaz-kiker <chaz-kiker@lambdastudents.com>
2020-10-16 15:04:26 -0500

    add test for text-based inputs

--- cypress/integration/pizza_app.spec.js
+++ cypress/integration/pizza_app.spec.js
@@ -2,6 +2,18 @@
 
 describe("MVP tests", () => {
 	beforeEach(() => {
-		cy.visit("http://localhost:3000");
+		cy.visit("http://localhost:3000/pizza-form");
+	})
+
+	const nameIn = () => cy.get("input[name='name']");
+	const instructIn = () => cy.get("textarea[name='instructions']");
+
+
+	describe("can add text to text inputs", () => {
+		it("can add text to text-based inputs", ()=>{
+			nameIn().should("have.value", "").type("Text").should("have.value", "Text");
+			instructIn().should("have.value", "").type("lorem ipsum lorem ipsum text here, here, and here").should("have.value", "lorem ipsum lorem ipsum text here, here, and here");
+		})
+		
 	})
 })
