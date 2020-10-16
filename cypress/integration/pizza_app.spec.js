e0b29f0c018313886847411390569487fb2d83a9
chaz-kiker <chaz-kiker@lambdastudents.com>
2020-10-16 15:30:02 -0500

    add test for submit button functionality

--- cypress/integration/pizza_app.spec.js
+++ cypress/integration/pizza_app.spec.js
@@ -9,6 +9,10 @@ describe("MVP tests", () => {
 
 	const nameIn = () => cy.get("input[name='name']");
 	const instructIn = () => cy.get("textarea[name='instructions']");
+	const sizeIn = () => cy.get("select[name='size']");
+	const redSauceIn = () => cy.get("input[value='og-red']");
+	const glutenFreeIn = () => cy.get("label[class='sc-gtssRu jyJnU']");
+	const submitBtn = () => cy.get("#submitBtn");
 	const pepperoniCheckbox = () => cy.get("input[name='pepperoni']");
 	const sausageCheckbox = () => cy.get("input[name='sausage']");
 	const onionsCheckbox = () => cy.get("input[name='onions']");
@@ -23,5 +27,20 @@ describe("MVP tests", () => {
 			sausageCheckbox().should("not.be.checked").check().should("be.checked");
 			onionsCheckbox().should("not.be.checked").check().should("be.checked");
 		})
+	})
+	describe.only("can submit the form when all inputs are filled correctly", () => {
+		it.only("submit works", () => {
+			submitBtn().should("be.disabled");
+			nameIn().type("name test");
+			sizeIn().select("lg");
+			redSauceIn().check();
+			glutenFreeIn().click();
+			pepperoniCheckbox().check();
+			sausageCheckbox().check();
+			cy.contains("div[class='order']").should("not.exist");
+			submitBtn().should("not.be.disabled").click();
+			// cy.contains("div.order").should("exist");
+			cy.get("div[class='order']").should("exist");
+		}) 
 	})
 })
